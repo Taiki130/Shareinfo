8826bbf1331fe462c6d48f691ba4e9cb579fb43e
Taiki Noda <air19990130@yahoo.co.jp>
2019-12-19 12:49:20 +0900

    errorの修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,7 @@ class Post < ApplicationRecord
   before_validation :set_no_title
 
   validates :title, presence: true, length: { maximum: 50 }
-  validates :description, presence: true, length: { maximum: 15000 }
+  validates :description, presence: false, length: { maximum: 15000 }
 
   belongs_to :user
   has_many :comments, dependent: :destroy
